34332d33385375c477390ce4a2d04682229e88d5
NICANORKYAMBA <nicanorkyamba98@gmail.com>
2023-03-02 11:31:29 +0300

    rotate binary tree right

--- 104-binary_tree_rotate_right.c
+++ 104-binary_tree_rotate_right.c
@@ -17,13 +17,13 @@ binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
 	tmp = pivot->right;
 	pivot->right = tree;
 	tree->left = tmp;
-	
+
 	if (tmp)
 		tmp->parent = tree;
 	tmp = tree->parent;
 	tree->parent = pivot;
 	pivot->parent = tmp;
-	
+
 	if (tmp)
 	{
 		if (tmp->left == tree)
